f4e2c2fc62f15d2846081171cf5ca0c387aecb35
Jaskiran <jaskiranchhokar@gmail.com>
2020-05-14 14:45:42 -0400

    Add generateModal Function to script.js

--- js/script.js
+++ js/script.js
@@ -42,11 +42,37 @@ function employeeInfo(employee) {
     </div>`; 
 }
 
+function generateModal() {
+    const body = document.querySelector('body'); 
+    body.innerHTML = `
+    <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+            <h3 id="name" class="modal-name cap">name</h3>
+            <p class="modal-text">email</p>
+            <p class="modal-text cap">city</p>
+            <hr>
+            <p class="modal-text">(555) 555-5555</p>
+            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+            <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>
+    </div>
+
+    <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+    </div>
+    `; 
+}
+
 function cardInteract() {
     const cards = document.querySelectorAll('.card'); 
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            console.log('hi');
+            generateModal();
         }); 
     }); 
 }
